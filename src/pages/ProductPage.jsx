3c188a72ad6e27908b3b2a58bfc7a174298c1237
Kai-Uwe Haase <Kaiuwehaase@hotmail.de>
2025-04-02 20:22:50 +0200

    🚀 Initial clean upload

---
+++ src/pages/ProductPage.jsx
@@ -0,0 +1,8 @@
+export default function ProductPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Produkt-Bereich</h1>
+      <p>Teile Links zu Produkten und lass sie bewerten.</p>
+    </div>
+  );
+}
---
+++ src/pages/VideoPage.jsx
@@ -0,0 +1,8 @@
+export default function VideoPage() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Video-Bereich</h1>
+      <p>Hier kannst du YouTube-Videos teilen und bewerten.</p>
+    </div>
+  );
+}
