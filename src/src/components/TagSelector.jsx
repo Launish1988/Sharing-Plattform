855518052ba36758b2ff3bfdb2ada25bd73d2f54
Launish1988 <kaiuwehaase@hotmail.de>
2025-04-06 12:18:46 +0200

    Update TagSelector.jsx

--- src/src/components/TagSelector.jsx
+++ src/src/components/TagSelector.jsx
@@ -1,1 +1,20 @@
 
+import React from "react";
+
+export default function TagSelector({ selected, setSelected }) {
+  const tags = ["Hammer Video", "Lustig", "Lerninhalt", "Highlight"];
+
+  return (
+    <select
+      value={selected}
+      onChange={(e) => setSelected(e.target.value)}
+      className="px-4 py-2 rounded bg-gray-800 text-white"
+    >
+      {tags.map((tag) => (
+        <option key={tag} value={tag}>
+          {tag}
+        </option>
+      ))}
+    </select>
+  );
+}
