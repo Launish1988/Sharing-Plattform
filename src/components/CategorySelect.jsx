dc7d2b6495698ff848c84b740a444fd9617f9067
Launish1988 <kaiuwehaase@hotmail.de>
2025-04-06 12:50:02 +0200

    Update CategorySelect.jsx

--- src/components/CategorySelect.jsx
+++ src/components/CategorySelect.jsx
@@ -1,1 +1,21 @@
 
+
+import React from "react";
+
+export default function CategorySelect({ category, setCategory }) {
+  const categories = ["Gaming", "Sport", "News", "Musik", "Wissen"];
+
+  return (
+    <select
+      value={category}
+      onChange={(e) => setCategory(e.target.value)}
+      className="px-4 py-2 rounded bg-gray-800 text-white"
+    >
+      {categories.map((cat) => (
+        <option key={cat} value={cat}>
+          {cat}
+        </option>
+      ))}
+    </select>
+  );
+}
