edd319183278bb103cff0447dbaf43622a5982bf
Launish1988 <kaiuwehaase@hotmail.de>
2025-04-06 12:20:13 +0200

    Update DraggableCard.jsx

--- src/src/components/DraggableCard.jsx
+++ src/src/components/DraggableCard.jsx
@@ -1,1 +1,26 @@
 
+import React from "react";
+
+export default function DraggableCard({ video, onArchive, children }) {
+  const handleDragStart = (e) => {
+    e.dataTransfer.setData("text/plain", video.id);
+  };
+
+  return (
+    <div
+      draggable
+      onDragStart={handleDragStart}
+      className="cursor-move border border-gray-700 rounded-lg overflow-hidden shadow-lg"
+    >
+      {children}
+      <div className="flex justify-end p-2">
+        <button
+          onClick={onArchive}
+          className="text-sm text-red-400 hover:underline"
+        >
+          Archivieren
+        </button>
+      </div>
+    </div>
+  );
+}
