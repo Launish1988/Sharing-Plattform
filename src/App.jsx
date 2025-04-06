46f5ebe2573cb0efc54bcca0a50117bb7511b0d8
Launish1988 <kaiuwehaase@hotmail.de>
2025-04-06 12:55:51 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import VideoPage from "./pages/VideoPage";
 import ProductPage from "./pages/ProductPage";
@@ -7,18 +7,11 @@ import ProductPage from "./pages/ProductPage";
 function App() {
   return (
     <Router>
-      <div className="p-4">
-        <nav className="flex gap-4 mb-4">
-          <Link to="/" className="font-bold">Dashboard</Link>
-          <Link to="/videos">Videos</Link>
-          <Link to="/produkte">Produkte</Link>
-        </nav>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/videos" element={<VideoPage />} />
-          <Route path="/produkte" element={<ProductPage />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/videos" element={<VideoPage />} />
+        <Route path="/produkte" element={<ProductPage />} />
+      </Routes>
     </Router>
   );
 }
