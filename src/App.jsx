7efa4e1343d5fad8795addfbd35e3853f3b7f967
Launish1988 <kaiuwehaase@hotmail.de>
2025-04-06 18:25:18 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-// Datei: src/App.jsx
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import VideoPage from "./pages/VideoPage";
-import ProductPage from "./pages/ProductPage";
 
-export default function App() {
+function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/videos" element={<VideoPage />} />
-        <Route path="/produkte" element={<ProductPage />} />
-      </Routes>
+      <div className="p-4">
+        <nav className="flex gap-4 mb-4">
+          <Link to="/" className="font-bold">Dashboard</Link>
+          <Link to="/videos">Videos</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/videos" element={<VideoPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
+
+export default App;
